88ee8252bfe5d206110048ba36f1a8dbd8c34c43
Suksham54 <76278896+Suksham54@users.noreply.github.com>
2021-03-18 21:59:42 +0530

    Adding Hello World to repository

--- 1_Hello.c
+++ 1_Hello.c
@@ -1,5 +1,7 @@
-#include <stdio.h>
-int main() {
-   printf("Hello World");
-   return 0;
+//Name: Suksham Gupta 
+//Program: Hello World
+	#include <stdio.h> //Pre-process directive to include standard input and output functions header file
+	int main() {        //Main Function
+ 	 	printf("Hello World\n");   //Printf function calling (to print hello world)
+ 	 	return 0; //return function
 }
