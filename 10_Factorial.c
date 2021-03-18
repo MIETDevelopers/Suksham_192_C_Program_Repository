4db0d83a887705368086fee62e147054a8d7efa8
Suksham54 <76278896+Suksham54@users.noreply.github.com>
2021-03-18 23:56:51 +0530

    add Factorial to repository

---
+++ 10_Factorial.c
@@ -0,0 +1,16 @@
+//Program to find factorial using for loop
+#include<stdio.h>   //Pre-process directive to include standard input and output functions header file 
+
+int main()     //Main function
+{
+  int i,f=1,num;            //variable declaration using int datatype
+ 
+  printf("Enter an integer");   //printf function calling
+  scanf("%d",&num);      //scanf function calling to receive input
+ 
+  for(i=1;i<=num;i++)  //for loop started 
+      f=f*i;
+ 
+  printf("Factorial of %d is: %d",num,f);  //print statement should be according to Question
+  return 0; //return function
+}
